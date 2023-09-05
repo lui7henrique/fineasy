9f9f1d99a2a9b924574b609d6926863117467eb9
Luiz Henrique <7henrique18@gmail.com>
2023-09-04 22:09:14 -0300

    fix: lint errors replacements

--- src/app/plain/new/new-plain-form/index.tsx
+++ src/app/plain/new/new-plain-form/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm, FormProvider } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 
 import { Button } from '@/components/ui/button'
